abf7ed86e2d3ec1e8b3c2e10584a34fe8ffded9d
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-12 14:38:34 +0200

    add devDependencies gulp

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+const gulp = require('gulp'),
+      compass = require('gulp-compass'),
+      sass = require('gulp-sass'),
+      browserSync = require('browser-sync'),
+      reload = browserSync.reload,
+      nodemon = require('gulp-nodemon');
--- server.js
+++ server.js
@@ -9,22 +9,20 @@ let app = express();
 // fait tourner le moteur ejs
 app.set('view engine', 'ejs');
 
-// utilise le dossier public pour les fichiers css, script.js
+// utilise le dossier public pour les fichiers statiques
 app.use(express.static('public'));
 
 // utilise le body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-// Lancement serveur sur le port 8080
-app.listen(process.env.PORT || 8080);
-// verifie dans la console si le serveur est lancé
-console.log("Serveur online");
-
-
-
 //appel de ma page d'accueil index.ejs
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+
+const server = app.listen(process.env.PORT || 8080, (req, res) => {
+    console.log('Server online!');
 });
 
