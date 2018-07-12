e7b7d9ebb8f7b0bdca599194635694f26a495723
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-12 14:43:44 +0200

    delet compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require('gulp'),
-      compass = require('gulp-compass'),
       sass = require('gulp-sass'),
       browserSync = require('browser-sync'),
       reload = browserSync.reload,
