1d377b9d1e3bbd3344e11fc9a3a1bfcbd115b1c4
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-14 09:16:46 +0200

    add carousel-slider

--- js/materialize.js
+++ js/materialize.js
@@ -4,3 +4,27 @@ document.addEventListener('DOMContentLoaded', function () {
         draggable: true
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+        fullWidth: true,
+        // indicators: true,
+        duration: 400
+    });
+});
+
+
+$('.carousel').carousel();
+setInterval(function () {
+    $('.carousel').carousel('next');
+}, 5000);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {
+        respponsivThreshold: 0
+    });
+});
