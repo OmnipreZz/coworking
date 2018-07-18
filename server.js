9b58baa9c83f14652e0cf80bc9b25c1fc61dc332
Edmond de Tournadre <edmond92@gmail.com>
2018-07-18 19:49:52 +0200

    compare users logging in to database users and redirect in consequence (no encryption yet)

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const express = require('express'),
 	  apikey = require('./sendgrid/apikey'),
 	  mysql = require('mysql'),
 	  bcrypt = require('bcrypt'),
-      sgMail = require('@sendgrid/mail');
-      connection = require('./public/js/connection')
+      sgMail = require('@sendgrid/mail'),
+      connection = require('./public/js/connection'),
+      jwt = require('json-web-token'),
+      config = require('./config');
+
 sgMail.setApiKey(apikey);
 
 // declare la variable app avec express
@@ -13,6 +16,9 @@ let app = express();
 
 // fait tourner le moteur ejs
 app.set('view engine', 'ejs');
+
+//set secret key to read token
+app.set("superSecret", config.secret);
 
 // utilise le dossier public pour les fichiers statiques
 app.use(express.static('public'));
@@ -46,11 +52,6 @@ app.post('/', (req, res) => {
 	res.render('index');
 });
 
-// page resa
-app.get('/resa', (req,res)=>{
-    res.render('resa');
-});
-
 // page test
 app.get('/test', (req,res)=>{
     res.render('testpage');
@@ -68,15 +69,12 @@ app.post('/registration', (req, res) => {
 	}
 	// création d'une variable avec la valeur du champs de confirmation du password
 	let confpwd = req.body.confpwd
-	// console.log(user.password);
-	// console.log(confpwd);
 	//requête pour récupérer le mail du nouveau user s'il existe déjà dans la DB
 	let queryMail = `SELECT mail FROM users WHERE mail = '${user.mail}'`;
 	// comparaison mdp/confirmation mdp && mail du nouveau user/mail dans DB
 	if (user.password == confpwd && queryMail != user.mail) {
 		// envoi du nouvel utilisateur
 		connection.query(`INSERT INTO users SET ?`, user);
-		connection.end();
 		const registrationmsg = {
 		to: user.mail,
 		from: "coworkingmda@gmail.com",
@@ -92,8 +90,51 @@ app.post('/registration', (req, res) => {
 	}
 
 });
-//---------------------------------------------
 
+
+// page resa
+app.get('/resa', (req,res)=>{
+	let uQuery = `SELECT name, surname, mail, role FROM Users WHERE name = "Djohn"`
+	connection.query(uQuery, (err, result)=>{
+		if(err){
+			console.error(err);
+		}
+		else {
+			console.log(result[0]);
+			console.log(result[0].name);
+		}
+	});
+    res.render('resa');
+});
+
+app.post('/dashboard', (req,res)=>{
+	let user = {
+		mail : req.body.mailRegister,
+        password : req.body.pwd
+	}
+
+	console.log(user.password);
+	let authquery = `SELECT * FROM Users WHERE mail ='${user.mail}'`
+	connection.query(authquery, (err, result)=>{
+		if(err){
+			console.error(err);
+		}
+		else if(result[0].mail == user.mail){
+			console.log(result);
+			console.log("vous existez dans la DB félicitation !!");
+			res.render("dashboard");
+		}
+		else {
+			console.log(result[0].mail);
+			console.log("Va t'inscrire noob");
+			connection.end();
+			res.redirect("/");
+		}
+	});
+});
+
+
+//---------------------------------------------
 
 const server = app.listen(process.env.PORT || 8080, (req, res) => {
     console.log('Server online!');
