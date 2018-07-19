759de33b63733159ee128ad5a068414b2d448467
Omniprezz <geoffrey.frioli@gmail.com>
2018-07-19 11:18:03 +0200

    fonction recuperer options resa

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Resa whithout registration
 //------------------------------
-$('.formula').click (function(){
+$('.formula').click(function() {
     let formulinfo = $(this).attr('formula'); 
     console.log(formulinfo);
     $("#msginfo").html('Vous avez choisi la formule : ');
@@ -11,3 +11,43 @@ $('.formula').click (function(){
 //------------------------------
 // End resa whithout registration
 
+
+
+// modal resa calendar checked ///////////////////////////
+
+$('.validModalResa').click(() => {
+
+    if ($('#morning').is(':checked')) {
+        var matin = "Matin";
+    }
+    if($('#afternoon').is(':checked')) {
+        var aprem = "Après-midi";
+    }
+    if ($('#cowork').is(':checked')) {
+        var espace = "Espace Coworking";
+    }
+    if ($('#solo').is(':checked')) {
+        var espace = "Bureau";
+    }
+    if ($('#screen').is(':checked')) {
+        var ecran = "Ecran";
+    }
+    if ($('#hammock').is(':checked')) {
+        var hamac = "Hamac";
+    }
+    console.log("option");
+    $("#msgOption").html('Vous avez choisi les options suivantes : ');
+    $("#optionEspace").html("-" + " " + espace);
+    $("#optionEcran").html("-" + " " + ecran);
+    $("#optionEcran").attr('name', ecran);
+    $("#optionHamac").html("-" + " " + hamac);
+    $("#optionHamac").attr('name', hamac);
+
+    $("#msgDate").html('Vous venez le : ');
+    $("#dateMatin").html("-" + " " + matin);
+    $('#dateMatin').attr('name', matin);
+    $("#dateAprem").html("-" + " " + aprem);
+    $('#dateAprem').attr('name', aprem);
+
+});
+//-----------------------------------------------------------
